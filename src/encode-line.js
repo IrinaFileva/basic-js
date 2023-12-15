d1305b343ea7b8f6dbd3935daaec684b255ef23a
Irina Fileva <irinkaskai@gmail.com>
2023-12-15 22:55:35 +0500

    feat: do the tack encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function encodeLine(str) {
+  let map = new Map();
+  let result = [];
+  let arr = str.split('');
+  let total = '';
+  let arr0 = arr[0];
+  let arr1 = arr.at(-1);
+
+  if(str === total) return str;
+
+  for(let item of arr){
+    if(map.has(item)){
+      map.set(item, map.get(item) + 1);
+    }else{
+      map.set(item, 1);
+    };
+  };
+
+  result = [...map.entries()];
+
+  result.map(x => x.reverse());
+    
+  for( let key of result){
+     total = total + key;
+  };
+
+  if(arr0 === arr1){
+     return total.replaceAll(',','').replaceAll('1','').slice(1) + arr1;
+  };
+  
+  return total.replaceAll(',','').replaceAll('1','');   
+ }
+
 
 module.exports = {
   encodeLine
