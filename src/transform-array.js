60283cf8618a9c05ee91f6ad24ee25d6cf307d5d
Irina Fileva <irinkaskai@gmail.com>
2023-12-17 11:07:36 +0500

    feat: do the tack transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (Array.isArray(arr)){
+
+    let arrTransform = [...arr];
+    let doublePrev = arrTransform[arrTransform.indexOf('--double-prev')-1];
+
+    if(arrTransform.includes('--double-next') && arrTransform.includes('--double-prev')){
+      arrTransform.splice(arrTransform.indexOf('--double-prev') -2, 3, doublePrev, doublePrev, doublePrev);                       
+    }
+    else if(arrTransform.includes('--double-prev') && arrTransform.indexOf('--double-prev') !== 0){
+      arrTransform.splice(arrTransform.indexOf('--double-prev') -2, 3);
+    }
+    else if(arrTransform.indexOf('--double-prev') === 0){
+      arrTransform.splice(arrTransform.indexOf('--double-prev'), 1);
+    }
+    else if(arrTransform.includes('--double-next') && arrTransform.includes('--discard-prev')){
+      arrTransform.splice(arrTransform.indexOf('--discard-prev') -2, 3, arrTransform[arrTransform.indexOf('--discard-prev')-1],);                       
+    }
+    else if(arrTransform.includes('--double-next') && arrTransform.at(-1) !== '--double-next'){
+      arrTransform.splice(arrTransform.indexOf('--double-next'), 1, (arrTransform.indexOf('--double-next')+1));
+    }
+    else if(arrTransform.at(-1) === '--double-next'){
+      arrTransform.splice(arrTransform.indexOf('--double-next'), 1);
+    }
+    else if(arrTransform.includes("--discard-prev") && arrTransform.indexOf('--discard-prev')!== 0){
+      arrTransform.splice((arrTransform.indexOf('--discard-prev') -1), 2);
+    }
+    else if(arrTransform.indexOf('--discard-prev')=== 0){
+      arrTransform.splice(arrTransform.indexOf('--discard-prev'), 1);
+    }
+    else if(arrTransform.includes('--discard-next') && arrTransform.at(-1) !== '--discard-next'){
+      arrTransform.splice(arrTransform.indexOf('--discard-next') -2, 2);
+    }
+    else if(arrTransform.at(-1) === '--discard-next'){
+     arrTransform.splice(arrTransform.indexOf('--double-next'), 1);
+    }
+  return arrTransform.filter(x => x !==  "--discard-next")
+  } 
+throw new Error('\'arr\' parameter must be an instance of the Array!');           
 }
+
+
+
 
 module.exports = {
   transform
