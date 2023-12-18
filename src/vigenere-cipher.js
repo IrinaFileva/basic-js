3eaa2eae4af39eafd51f22a52e4e137eaeffa275
Irina Fileva <irinkaskai@gmail.com>
2023-12-18 19:18:44 +0500

    feat: do the tack vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,104 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alf = 'abcdefghijklmnopqrstuvwxyz';
+
+  constructor(straight = true){
+    this.straight = straight;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+
+  encrypt(message, key) {
+
+    if (message === undefined || key === undefined){
+      throw new Error ('Incorrect arguments!');
+    };
+
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    if(message.length > key.length){
+      key = key.padEnd(message.length, key);
+    }
+    else if(key.length > message.length){
+      key = key.slice(0, message.length);
+    };
+    
+    let n = 0;
+    let result = [];
+
+    for(let i = 0; i < message.length; i++){
+
+      let valMessage = message[i];
+      let valKey = key[i - n];
+
+      if(!(this.alf.includes(valMessage))){
+        result.push(valMessage);
+        n++;
+      }
+      else{
+        let indMessage = this.alf.indexOf(valMessage);
+        let indKey = this.alf.indexOf(valKey);
+        let index = (indMessage + indKey) % 26;
+        let item = this.alf[index];
+          result.push(item);
+      };
+    };
+
+    if(!this.straight){
+      return result.reverse().join('').toUpperCase();
+    }
+    
+    return result.join('').toUpperCase();   
+  }; 
+
+  decrypt(message, key) {
+
+    if (message === undefined || key === undefined){
+      throw new Error ('Incorrect arguments!');
+    };
+
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    if(message.length > key.length){
+      key = key.padEnd(message.length, key);
+    }
+    else if(key.length > message.length){
+      key = key.slice(0, message.length);
+    };
+    
+    let n = 0;
+    let result = [];
+
+    for(let i = 0; i < message.length; i++){
+
+      let valMessage = message[i];
+      let valKey = key[i - n];
+
+      if(!(this.alf.includes(valMessage))){
+        result.push(valMessage);
+        n++;
+      }
+      else{
+        let indMessage = this.alf.indexOf(valMessage);
+        let indKey = this.alf.indexOf(valKey);
+        let index = (indMessage - indKey) % 26;
+        if(Math.sign(index) === -1){
+          index = (index + 26) % 26;
+        };
+        let item = this.alf[index];
+          result.push(item);
+       };
+      };
+
+      if(!this.straight){
+        return result.reverse().join('').toUpperCase();
+      };
+    
+    return result.join('').toUpperCase();
+  };
+};
+
 
 module.exports = {
   VigenereCipheringMachine
